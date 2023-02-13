764e1021fce14626e1b33cce83a3216d0d96b04c
DESKTOP-052EG82\KGA_22 <im5820795@naver.com>
2023-02-13 12:50:55 +0900

    [Junil] Init Dungreed game project

---
+++ Dungreed/Assets/01.UnityProject/Scripts/Global/GData+Define.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static partial class GData
+{
+    public const string SCENE_NAME_TITLE = "01.TitleScene";
+
+    public const string SCENE_NAME_LOAD = "02.LoadScene";
+
+    public const string SCENE_NAME_PLAY = "03.PlayScene";
+
+    public const string SCENE_NAME_LOAD2 = "04.LoadScene2";
+
+    public const string SCENE_NAME_PLAY2 = "05.PlayScene2";
+
+
+    public const string OBJ_NAME_CURRENT_LEVEL = "Level_1";
+
+    public const string GAME_OBJS = "GameObjs";
+
+    public const string MAIN_OBJS = "MainObjs";
+
+    public const string PLAYER_OBJS = "Player";
+
+
+}
+
+
+
+public enum PuzzleType
+{
+    NONE = -1,
+    PUZZLE_BIG_TRIANGLE
+}   // PuzzleType
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Dungreed
 Make a Dungreed game project
+
+2023-02-13 / v0.0.0 / Init Dungreed game project    
